2a523e9fb5357b38271c706b3c49fdac0be1fc9c
Alex <s4alwiss@uni-trier.de>
2025-05-07 14:21:50 +0200

    task C

---
+++ Assets/Scripts/AufgabenteilC/SphereController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class SphereController : MonoBehaviour
+{
+
+    [Range(-180, 180)]
+    [Tooltip("Meter per second")]
+    public float rotationSpeed = 90;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        //rotate around the vertical axis by 90 degrees and within one second
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+    }
+}
